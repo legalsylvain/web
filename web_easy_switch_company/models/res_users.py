4e1ab0cf6636083c528ce165300f5ebb5c945362
Sylvain LE GAL <sylvain.legal@grap.coop>
2019-09-29 18:52:06 +0200

    [WIP] add web_easy_swith_company

---
+++ web_easy_switch_company/models/res_users.py
@@ -0,0 +1,12 @@
+# Copyright (C) 2014-Today: GRAP (http://www.grap.coop)
+# @author: Sylvain LE GAL (https://twitter.com/legalsylvain)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from odoo import models, fields
+
+
+class ResUsers(models.Model):
+    _inherit = 'res.users'
+
+    favorite_company_ids = fields.Many2many(
+        string="Favorite Companies", comodel_name="res.company")
